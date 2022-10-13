d196fa2f3c3795a9bcb9f0577e671e370e48b3e6
Olisa-tech <Okoloaustin16@gmail.com>
2022-10-13 20:52:16 +0800

    hamburgerstarted

---
+++ starter/main.js
@@ -0,0 +1,17 @@
+
+
+// es5
+// console.log("send me your account number sharp")
+// var writeup = "I am tired"
+
+// function Itisforeba() {
+// alert(writeup)
+// }
+var hamburgerdiv = document.querySelector(".hamburger")
+
+console.log(hamburgerdiv)
+
+function Itisforeba() {
+    hamburgerdiv.classList.toggle("showburger")
+
+}
